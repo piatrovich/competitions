0ab9989112310d66ccc2cd628eeec832026c3589
Dzmitry Piatrovich <piatrovich@live.ru>
2023-03-11 23:13:25 -0600

    206. Reverse Linked List | add iterative fast solution

--- LeetCode/P206-reverse-linked-list/src/main/java/com/piatrovich/leetcode/IterativeSolution.java
+++ LeetCode/P206-reverse-linked-list/src/main/java/com/piatrovich/leetcode/IterativeSolution.java
@@ -1,2 +1,20 @@
-package com.piatrovich.leetcode;public class IterativeSolution {
+package com.piatrovich.leetcode;
+
+class IterativeSolution {
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+
+        ListNode prev = null;
+        while (head.next != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        head.next = prev;
+
+        return head;
+    }
+
 }
